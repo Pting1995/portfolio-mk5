f0669d807963d9b120798de31afce8971f912279
Peter Ting <pting1995@gmail.com>
2021-09-14 12:08:18 -0700

    actually fixed routing issue by switching to hashrouter on react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as HashRouter, Route, Switch } from "react-router-dom";
 import OverviewProjectPage from "./pages/overviewProjectPage";
 import SingleProjectPage from "./pages/singleProjectPage";
 
@@ -9,18 +9,18 @@ import projectList from "./components/projectList.json"
 function App() {
   return (
     <>
-      <Router >
+      <HashRouter basename="portfolio-mk5">
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/'}>
+          <Route exact path={'/'}>
             <OverviewProjectPage projectList={projectList} />
           </Route>
-          <Route exact path={process.env.PUBLIC_URL + "/project/:projectName"}>
+          <Route exact path={"/project/:projectName"}>
             <SingleProjectPage projectList={projectList} />
           </Route>
         </Switch>
 
         {/* <Footer /> */}
-      </Router>
+      </HashRouter>
     </>
   );
 }
--- client/src/components/projectCard.js
+++ client/src/components/projectCard.js
@@ -7,7 +7,7 @@ function projectCard(props) {
     return (
         <>
             <div className={props.className}>
-                <Link to={`portfolio-mk5/project/${props.url}`}>
+                <Link to={`project/${props.url}`}>
                     <article className="project">
                         <figure>
                             <ImgDB imgName={props.imgName} />

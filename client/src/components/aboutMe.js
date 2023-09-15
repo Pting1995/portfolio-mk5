12772b54b458398110376069bf3c223895b9c7a5
Peter Ting <pting1995@gmail.com>
2023-09-14 22:24:43 -0700

    update text & touch up

--- client/src/components/aboutMe.js
+++ client/src/components/aboutMe.js
@@ -9,15 +9,15 @@ function AboutMe() {
                     <div className="about-me">
                         <nav>
                             <a href="/portfolio-mk5">My Projects</a>
-                            <a href="https://github.com/Pting1995" target="_blank">My Github</a>
-                            <a href="https://www.linkedin.com/in/pting002/" target="_blank">My LinkedIn</a>
+                            <a href="https://github.com/Pting1995" target="_blank" rel="noreferrer">My Github</a>
+                            <a href="https://www.linkedin.com/in/pting002/" target="_blank" rel="noreferrer">My LinkedIn</a>
                             <a href="/portfolio-mk5/assets/Resume-Peter-Ting.pdf" target="_blank">My Resume</a>
                         </nav>
                         <figure>
                             <ImgDB imgName="peter" />
                         </figure>
                         <h1>Hey there, I'm Peter!</h1>
-                        <p>I am currently seeking full-time opportunities in web development. Please contact me if any of my projects interest you!</p>
+                        <p>I fell in love with coding ever since my first "A-ha!" moment and am currently pursuing the next epiphany! Please contact me if any of my projects interest you.</p>
                     </div>
                 </div>
             </section>
